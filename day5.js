0fa87d915575ec5a518b78b9541cbbc82c700738
shouq46 <shouq0406@gmail.com>
2022-09-15 00:10:33 +0300

    done

---
+++ day5.js
@@ -0,0 +1,23 @@
+alert("Welcome to my website! I am Shouq and I will calculate your score");
+let grade = prompt('Enter your grade');
+console.log(grade) 
+
+
+  let number = 95 ;
+if ( grade > 90 && grade < 100 ) {
+    console.log ( "You got a high grade 🥳" ) ;
+} else if ( grade > 80 && grade < 89 ) {
+    console.log ( " You got very good 🤩" ) ;
+} else if ( grade > 70 && grade < 79) {
+    console.log ( " You got good 🙂" ) ;
+} else if ( grade > 60 && grade < 69) {
+    console.log ("You got accepted 😕"); 
+} else if ( grade > 50 && grade < 59) {
+    console.log ("You got weak ☹️");
+ } else if ( grade < 50 ) {
+    console.log ("You failed 💔");
+ }
+    
+
+
+
